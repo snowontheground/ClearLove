b824923cef0041570224fbebcc9a0d9e4d2cb31e
snowontheground <229290194@qq.com>
2019-01-11 19:33:30 +0800

    codeing by zyz

--- app/src/main/java/com/example/clearlove777/subjectmanager/UserActivity.java
+++ app/src/main/java/com/example/clearlove777/subjectmanager/UserActivity.java
@@ -3,11 +3,11 @@ package com.example.clearlove777.subjectmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_user);
     }
 }
